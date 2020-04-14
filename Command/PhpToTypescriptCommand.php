1632812d7184be48974c71aab8a3add8690820b6
Harald Bauhofer <snakedove@web.de>
2020-04-14 11:37:33 +0200

    rename file

--- Command/PhpToTypescriptCommand.php
+++ Command/PhpToTypescriptCommand.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class PhpToTypeScriptCommand extends Command
+class PhpToTypescriptCommand extends Command
 {
     private string $projectDir;
 
